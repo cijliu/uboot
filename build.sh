1bff134c76e7617ee32e94d52d7a33ef63defe91
cijliu <cijliu@qq.com>
2019-12-17 23:49:29 +0800

    update build.sh script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 make ARCH=arm CROSS_COMPILE=arm-himix100-linux- hi3516ev200_config
 make ARCH=arm CROSS_COMPILE=arm-himix100-linux- 
+cp reg_info.bin .reg
 make ARCH=arm CROSS_COMPILE=arm-himix100-linux- u-boot-z.bin
 
